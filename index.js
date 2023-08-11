1e0c2e5c68e7bb0f5dd9b6048b5a0ab66b4b1fc2
belw7898 <bella.wang.10@gmail.com>
2023-08-11 06:35:47 -0700

    a few changes before presentation

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const katakana = 'アァカサタナハマヤャラワガザダ';
 const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const nums = '0123456789';
 
 const alphabet = katakana + latin + nums;
 
-const fontSize = 12;
+const fontSize = 15;
 const columns = canvas.width/fontSize;
 
 const rainDrops = [];
